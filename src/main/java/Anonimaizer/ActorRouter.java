7e4d43554a49f1482698ea14fe2e2481b9e78d2a
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-24 22:49:26 +0300

    Scripted auto-commit on change (2021-12-24 22:49:26) by gitwatch.sh

--- src/main/java/Anonimaizer/ActorRouter.java
+++ src/main/java/Anonimaizer/ActorRouter.java
@@ -2,10 +2,7 @@ package Anonimaizer;
 
 import akka.NotUsed;
 import akka.actor.ActorRef;
-import akka.http.javadsl.model.HttpEntities;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.model.Query;
+import akka.http.javadsl.model.*;
 import akka.japi.Pair;
 import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
@@ -34,12 +31,25 @@ public class ActorRouter {
     private final static int TIMEOUT = 5000;
     private final static Duration TIMEOUT_DURATION = Duration.ofMillis(TIMEOUT);
 
+//    создаем с помощью api route в акка http сервер который принимает два параметра, и если счетчик не равен 0,
+//    то сначала получает новый урл сервера (от актора хранилища конфигурации)
+//    и делает запрос к нему с аналогичными query параметрами (url, counter) но счетчиком на 1 меньше.
+//    Либо осуществляет  запрос по url из параметра
+
+
     public Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer) {
         return Flow.of(HttpRequest.class)
                 .map(request -> {
                     Query query = request.getUri().query();
                     Optional<String> url = query.get(URL_QUERY);
                     Optional<String> count = query.get(REQUEST_NUMBER_QUERY);
+
+                    Integer requestNumber = Integer.parseInt(count.get());
+                    if (requestNumber == 0) {
+                        return
+                    }
+
+                    String newUrl = getNewUrl(url, count);
 
                     return new Pair<>(url.get(), count.get());
                 })
@@ -81,4 +91,10 @@ public class ActorRouter {
                     return param.getAvrTime();
                 }).toMat(Sink.head(), Keep.right());
     }
+
+    private static final String SERVER_URL
+
+    private String getUrlQuery(String url, Integer requestNumber) {
+      return Uri.create()
+    };
 }
