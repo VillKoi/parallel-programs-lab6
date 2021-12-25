a762c61b443b9e69a47786b3f4be08378d0a2b18
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-25 11:11:03 +0300

    Scripted auto-commit on change (2021-12-25 11:11:03) by gitwatch.sh

--- src/main/java/Anonimaizer/ZooK.java
+++ src/main/java/Anonimaizer/ZooK.java
@@ -24,7 +24,7 @@ public class ZooK implements Watcher {
     }
 
     public void createConnection(String port) throws KeeperException, InterruptedException {
-        this.zooKeeper.create(PATH + "/" + SERVER,
+        this.zooKeeper.create(PATH ,
                 port.getBytes(StandardCharsets.UTF_8),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL);
