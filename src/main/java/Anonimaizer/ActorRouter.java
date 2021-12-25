1199b87105904f520ab8e6530c30770e79be6fe2
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-25 10:28:07 +0300

    Scripted auto-commit on change (2021-12-25 10:28:07) by gitwatch.sh

--- src/main/java/Anonimaizer/ActorRouter.java
+++ src/main/java/Anonimaizer/ActorRouter.java
@@ -53,7 +53,7 @@ public class ActorRouter {
                                                             getNewUrl(serverUrl.toString(), url, newRequestNumber)
                                                     ))
                                     );
-                                }
+                                 }
                         ))));
     }
 
