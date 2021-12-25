968903b384e9c9caa2baf2dd03c14d08e92fcb21
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-25 13:04:49 +0300

    Scripted auto-commit on change (2021-12-25 13:04:49) by gitwatch.sh

--- src/main/java/Anonimaizer/StoreActor.java
+++ src/main/java/Anonimaizer/StoreActor.java
@@ -6,20 +6,21 @@ import akka.actor.ActorRef;
 import java.util.List;
 
 public class StoreActor extends AbstractActor {
-// создаем актор хранилище конфигурации.
+    // создаем актор хранилище конфигурации.
 // Он принимает две команды
 // -	список серверов (который отправит zookeeper watcher)
 // -	запрос на получение случайного сервера
     private List<String> servers;
 
     @Override
-    public AbstractActor.Receive createReceive(){
+    public AbstractActor.Receive createReceive() {
         return receiveBuilder(
         ).match(ServerList.class, servers -> {
                     System.out.println(servers.getServers());
                     this.servers = servers.getServers();
                 }
         ).match(RandomInt.class, random -> {
+                    System.out.println(servers.toString());
                     getSender().tell(servers.get(random.getInt(servers.size())), ActorRef.noSender());
                 }
         ).build();
